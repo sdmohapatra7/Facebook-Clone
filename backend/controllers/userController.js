a58549228c381476873c8c491f61e4408d57dd23
Shaktidhar <sdmohapatra7@gmail.com>
2023-08-18 14:15:09 +0530

    create jsonwebtoken and store in cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,24 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports.createUser = async (req, res) => {
     try {
         const existUser = await User.findOne({email:req.body.email});
         if (existUser) {
             return res.status(400).json({
+                success:false,
                 message: 'User Already Exist On this Mail'
             });
         }
         const user = await User.create(req.body);
         return res.status(201).json({
+            success:true,
             message: 'user created successfully',
             user,
         });
     } catch (error) {
         return res.status(500).json({
+            success:false,
             message:'Internal Server error',
             error
         });
@@ -26,31 +30,47 @@ module.exports.loginUser = async(req,res)=>{
         const {email,password,confirmPassword} = req.body;
         if(!email || !password){
             return res.status(400).json({
+                success:false,
                 message:'Please Enter Email And password'
             });
         }
         if(confirmPassword !== password){
             return res.status(400).json({
+                success:false,
                 message:'Invalid Username Password'
             });
         }
         const user = await User.findOne({email});
         if(!user){
             return res.status(400).json({
+                success:false,
                 message:'User Not found Please SignUp'
             });
         }
         const isMatchPassword = await user.comparePassword(password);
         if(!isMatchPassword){
             return res.status(400).json({
+                success:false,
                 message:'Invalid Username Password'
             });
         }
+        const token = jwt.sign({
+            data:{email:user.email}
+        },'secret', { expiresIn: '0.2h' });
+        //store the token in cookie
+        res.cookie("token",token,{
+            httpOnly:true,   // Allow only server-side access
+            maxAge:3600,     // Cookie expires after one hour (in seconds)
+            path: '/',       // Valid for the entire domain
+        });
         return res.status(200).json({
+            success:true,
             message:'Now You Assess What you need',
+            token,
         });
     } catch (error) {
         return res.status(500).json({
+            success:false,
             message:'Internal Server error',
             error
         });
