9f8ec3768e37b509cc1e43ad2b19ed5a7bcb500f
Shaktidhar <sdmohapatra7@gmail.com>
2023-08-18 13:16:40 +0530

    hashing the password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -22,6 +24,14 @@ const userSchema = new mongoose.Schema({
 
 },{
     timestamps:true
+});
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.model('User',userSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/signIn',userController.createUser);
+router.post('/signup',userController.createUser);
 
 module.exports = router;
